e42547eed01126becc6072e7d0c1dfc13d460fa8
det_zer0 <92924690+detzer0@users.noreply.github.com>
2025-08-05 15:37:03 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sNewton4PDEOpt",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],  # add dependencies here if any
+)
